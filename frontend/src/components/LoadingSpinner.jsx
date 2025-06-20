7a3c1f6e013cac2d57ec686e598f5011050c4efe
ayan-x1 <pathanayan8347@gmail.com>
2025-06-20 22:58:35 +0530

    final commit

--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 const LoadingSpinner = () => {
 	return (
-		<div className='min-h-screen bg-gradient-to-br from-gray-900 via-yellow-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'>
+		<div className='min-h-screen bg-gradient-to-r from-black via-gray-900 to-yellow-600 flex items-center justify-center relative overflow-hidden'>
 			{/* Loading Spinner */}
 			<motion.div
 				className='w-16 h-16 border-4 border-t-4 border-t-yellow-500 border-yellow-300 rounded-full'
